3be1b6110c425f5582fceb20d5da9051f290f141
nickgnd <nick.gnd@gmail.com>
2015-04-09 22:07:36 +0200

    Created simple Homepage#index. Created partial _topbar. Started the view of Organization#index

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,7 +1,7 @@
 class OrganizationsController < ApplicationController
 
   def index
-    @organizations = Organization.all
+    @organizations = Organization.limit(10)
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'homepage/index'
+
   get 'organizations/index'
 
   get 'organizations/show'
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'homepage#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
